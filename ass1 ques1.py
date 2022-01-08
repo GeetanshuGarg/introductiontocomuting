fc9e8041a16fd01e8e30076a8b87319768d5abfe
GeetanshuGarg <97080140+GeetanshuGarg@users.noreply.github.com>
2022-01-08 10:21:00 +0530

    Delete ass1 ques1.py

--- ass1 ques1.py
+++
@@ -1,7 +0,0 @@
-print("enter first number")
-n1 = input()
-print("enter second number")
-n2 = input()
-print("enter third number")
-n3 = input()
-print("average of the three numbers is", 1/3*(int(n1) + int(n2) + int(n3)))
