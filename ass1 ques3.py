cca32b581bffc286eb3b6509d4508cdcbea641e5
GeetanshuGarg <97080140+GeetanshuGarg@users.noreply.github.com>
2022-01-08 14:24:31 +0530

    Add files via upload

---
+++ ass1 ques3.py
@@ -0,0 +1,8 @@
+# Making a list of student data containg different data type values
+SID=21103045
+Name='Geetanshu Garg'
+Gender='M'
+Branch='CSE'
+CGPA=9.9
+studentdata = [SID,Name,Gender,Branch,CGPA]
+print(studentdata)
