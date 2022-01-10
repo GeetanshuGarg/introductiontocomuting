151ec124630ca1b423131f2d9e1768e32e41c92b
GeetanshuGarg <97080140+GeetanshuGarg@users.noreply.github.com>
2022-01-10 15:25:08 +0530

    Add files via upload

---
+++ output.py
@@ -0,0 +1,46 @@
+#output of assignment1 ques1
+
+enter first number
+67
+enter second number
+87
+enter third number
+56
+average of the three numbers is 70.0
+
+
+
+#output of assignment1 ques2
+
+your income
+50000
+no of dependemts
+2
+tax payable = 6800.0
+
+
+
+#output of assignment1 ques3
+
+[21103045, 'Geetanshu Garg', 'M', 'CSE', 9.9]
+
+
+
+#output of assignment1 ques4
+
+enter marks of student 1: 98
+enter marks of student 2: 97
+enter marks of student 3: 99
+enter marks of student 4: 96
+enter marks of student 5: 95
+['98', '97', '99', '96', '95']
+['95', '96', '97', '98', '99']
+
+
+
+#output of assignment1 ques5
+
+colour
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+colour
+['Red', 'Green', 'White', 'Purple', 'Purple', 'Yellow']
